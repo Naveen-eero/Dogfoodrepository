29ed845dd75977abd3f0c6eff97c6a210425eaa3
kunnavee <kunnavee@192.168.0.111>
2023-12-06 20:19:27 +0530

    Login

--- src/main/java/eero/dogfood/LoginPage.java
+++ src/main/java/eero/dogfood/LoginPage.java
@@ -1,5 +1,26 @@
 package eero.dogfood;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
 public class LoginPage {
+
+	AndroidDriver driver;
+
+	public LoginPage(AndroidDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+
+	@AndroidFindBy(id = "com.eero.android.dogfood:id/login_button")
+	private WebElement NewToEero;
+
+	@AndroidFindBy(id = "com.eero.android.dogfood:id/signup_button")
+	private WebElement alreadyHaveAccElement;
 
 }
