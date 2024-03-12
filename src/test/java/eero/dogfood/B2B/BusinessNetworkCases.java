daea5a09edd892c305483fbeca7576e0e9eb2aa2
kunnavee <kunnavee@10.5.85.31>
2024-03-12 16:14:34 +0530

    branch commit

--- src/test/java/eero/dogfood/B2B/BusinessNetworkCases.java
+++ src/test/java/eero/dogfood/B2B/BusinessNetworkCases.java
@@ -24,7 +24,7 @@ import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class BusinessNetworkCases extends BaseTest {
-
+//code from branch
 	// Create and enable Subnet A and configure it as Business network
 
 	@SuppressWarnings("deprecation")
